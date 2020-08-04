b66f4b18ed921ebc04458d36e9c6fc136047fae6
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-04 07:32:07 +0000

    Done.

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,13 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {patriarch:{}, matriarch:{}, hero:{}, hero_friends:[]},
+      
+  
+    :capulet => {:patriarch=>{},:matriarch=>{},:heroine=>{},:heroine_friends=> []
       #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+      }
+    
+  }
 
   
 end
